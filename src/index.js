72667949ed5b859821cc17eada0811922392106b
CJE <engels.carlos@gmail.com>
2025-05-01 16:19:08 -0600

    fix visual

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // /opt/website-sync/repo/src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
